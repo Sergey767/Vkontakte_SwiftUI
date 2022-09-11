36422c23f0dd7cb92eced0d0f9dc6f0560df66e9
Сергей Горячев <serezha_goryachev@list.ru>
2022-09-11 14:21:18 +0400

    Создал новый проект

---
+++ Vkontakte_SwiftUI/Vkontakte_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vkontakte_SwiftUIApp.swift
+//  Vkontakte_SwiftUI
+//
+//  Created by Сергей Горячев on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Vkontakte_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
